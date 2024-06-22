e56c585a2402cbd29c86b5116be9fb88c8874118
Jam Jes <77445181+jamjes@users.noreply.github.com>
2024-06-22 14:37:31 +0100

    //Snake
    - Snake size 3 will spawn on start
    
    - Snake can move using directional input
    
    - Snake movement will update every .2secs
    
    - Snake cannot move in opposite direction

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <raylib.h>
+#include <raymath.h>
+#include <deque>
 
 using namespace std;
 
@@ -8,6 +10,46 @@ Color darkGreen = Color{43, 51, 24, 255};
 
 int cellSize = 30;
 int cellCount = 25;
+
+double lastUpdateTime = 0;
+
+bool EventTriggered(double interval)
+{
+    double currentTime = GetTime();
+
+    if (currentTime - lastUpdateTime >= interval)
+    {
+        lastUpdateTime = currentTime;
+        return true;
+    }
+
+    return false;
+}
+
+class Snake 
+{
+    public:
+    deque<Vector2> body = {Vector2{6,9}, Vector2{5,9}, Vector2{4,9}};
+    Vector2 direction = Vector2{1,0};
+
+    void Draw()
+    {
+        for(unsigned int i = 0; i < body.size(); i++)
+        {
+            float x = body[i].x;
+            float y = body[i].y;
+            Rectangle segment = Rectangle{x * cellSize, y * cellSize, (float)cellSize, (float)cellSize};
+            DrawRectangleRounded(segment, .5, 6, darkGreen);
+        }
+    }
+
+    void Update()
+    {
+        body.pop_back();
+        body.push_front(Vector2Add(body[0], direction));
+    }
+
+};
 
 class Food 
 {
@@ -41,20 +83,62 @@ class Food
         }
 };
 
+class Game
+{
+    public:
+    Snake snake = Snake{};
+    Food food = Food{};
+
+    void Draw()
+    {
+        snake.Draw();
+        food.Draw();
+    }
+
+    void Update()
+    {
+        snake.Update();
+    }
+};
+
 int main() 
 {
     InitWindow(cellSize * cellCount, cellSize * cellCount, "Snake Game");
     SetTargetFPS(60);
 
-    Food apple = Food();
+    Game game = Game{};
 
     while (WindowShouldClose() == false)
     {
         BeginDrawing();
 
-        ClearBackground(green);
+        if (EventTriggered(.2))
+        {
+            game.Update();
+        }
 
-        apple.Draw();
+        if (IsKeyPressed(KEY_UP) && game.snake.direction.y != 1)
+        {
+            game.snake.direction = Vector2{0,-1};
+        }
+
+        if (IsKeyPressed(KEY_DOWN) && game.snake.direction.y != -1)
+        {
+            game.snake.direction = Vector2{0,1};
+        }
+
+        if (IsKeyPressed(KEY_RIGHT) && game.snake.direction.x != -1)
+        {
+            game.snake.direction = Vector2{1,0};
+        }
+
+        if (IsKeyPressed(KEY_LEFT)  && game.snake.direction.x != 1)
+        {
+            game.snake.direction = Vector2{-1,0};
+        }
+
+        ClearBackground(green);
+        game.Draw();
 
         EndDrawing();
     }
